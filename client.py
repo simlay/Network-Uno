e397fdb8f70bd399e00935fe5e4e02c24408f89d
Sebastian Imlay <sebastian.imlay@gmail.com>
2012-12-11 19:39:51 -0800

    Added support for manual client.

--- client.py
+++ client.py
@@ -205,6 +205,83 @@ class Client():
             self.playGame()
             #self.data = ""
 
+
+class Manual_Client():
+    def __init__(self, hostname, port, username):
+        self.hostname = hostname
+        self.port = port
+        self.username = username
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def run(self):
+        serverAddress = (self.hostname, self.port)
+        self.s.connect(serverAddress)
+        self.data = ""
+        self.myCards = []
+
+        while True:
+            readable , writable, exceptional = select.select([self.s], [], [], 1)
+            if len(readable) > 0:
+                new_data = readable[0].recv(1024)
+                if new_data:
+                    self.data = self.data + new_data
+                else:
+                    sys.exit(0)
+
+            # GOOD GAME?
+            m = re.search("(?<=\[GG\|)[a-zA-Z0-9_]+", self.data)
+            if m:
+                self.data = re.sub("\[GG\|[a-zA-Z0-9_]+\]", "", self.data)
+                print "PLAYER %s WON!" % m.group(0)
+
+            # NEW GAME?
+            m = re.search("(?<=\[STARTGAME\|)([a-zA-Z0-9_]+\,?)*", self.data)
+            if m:
+                self.playerList = m.group(0).split(",")
+                self.data = re.sub("\[STARTGAME\|([a-zA-Z0-9_]+\,?)*\]", "", self.data)
+                print "STARTING A GAME WITH %s" % self.playerList
+                #break
+
+            m = re.search("(?<=\[PLAYERS\|)([a-zA-Z0-9]+\,?)+", self.data)
+            if m:
+                self.playerList = m.group(0).split(",")
+                self.data = re.sub("\[PLAYERS\|([a-zA-Z0-9_]+\,?)+\]", "", self.data)
+
+            # HAVE I BEEN DEALT?
+            m = re.search("(?<=\[DEAL\|)([A-Z0-9]+\,?)+", self.data)
+            if m:
+                newCards = m.group(0).split(",")
+                self.myCards = self.myCards + newCards
+                print "MY CARDS ARE %s, newCards are %s" % (self.myCards, newCards)
+                self.data = re.sub("\[DEAL\|([A-Z0-9]+\,?)+\]", "", self.data)
+
+            # Did someone play a card?
+            m = re.search("(?<=\[PLAYED\|)[a-zA-Z0-9_]+,[A-Z0-9]+", self.data)
+            while m:
+                playerName, cardToRemove = m.group(0).split(",")
+                if playerName == self.username and cardToRemove != "NN":
+                    if cardToRemove[1] == "F" or cardToRemove[1] == "W":
+                        cardToRemove = "N" + cardToRemove[1]
+
+                print "%s played card %s" % (playerName, cardToRemove)
+                self.data = re.sub("\[PLAYED\|[a-zA-Z0-9_]+,[A-Z0-9]+\]", "", self.data)
+                m = re.search("(?<=\[PLAYED\|)[a-zA-Z0-9_]+,[A-Z0-9]+", self.data)
+
+            # IS IT A TURN?
+            m = re.search("(?<=\[GO\|)[A-Z0-9]+", self.data)
+            if m:
+                topCard = m.group(0)
+                self.data = re.sub("\[GO\|[A-Z0-9]+\]", "", self.data)
+                #print "MY TURN!!!!", topCard, self.data
+                self.makeTurn(topCard)
+
+            # UNO?
+            m = re.search("(?<=\[UNO\|)[a-zA-Z0-9_]+", self.data)
+            if m:
+                winner = m.group(0)
+                self.data = re.sub("\[UNO\|[a-zA-Z0-9_]+\]", "", self.data)
+
+
 if __name__ == "__main__":
     #parser = OptionParser()
     parser = OptionParser(conflict_handler="resolve")
